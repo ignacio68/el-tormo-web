027ba985ccf195668acabeb48371669628cc86bc
Ignacio López-Amor <ilopez-amor@ya.com>
2024-06-21 16:40:49 +0200

    fix: change init page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,12 @@
-<template>
-  <TheNavBar></TheNavBar>
-  <HomeView></HomeView>
-  <TheFooter></TheFooter>
-</template>
 <script setup lang="ts">
 import TheNavBar from '@/components/TheNavBar.vue'
 import HomeView from '@/components/HomeView.vue'
 import TheFooter from '@/components/TheFooter.vue'
 </script>
+<template>
+  <div></div>
+  <!-- <TheNavBar></TheNavBar>
+  <HomeView></HomeView>
+  <TheFooter></TheFooter> -->
+</template>
 <style lang="sass" scoped></style>
