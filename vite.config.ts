524a3fc9698d0804fa9d59b06e1226f539ed5557
Ignacio López-Amor <ilopez-amor@ya.com>
2024-06-27 19:17:21 +0200

    core: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve, dirname } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools(),
+    VueI18nPlugin({
+      runtimeOnly: false, // <--- add this
+      include: resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        './src/i18n/locales/**'
+      )
+    })
   ],
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
