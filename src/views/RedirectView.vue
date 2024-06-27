d9807c8eb5197029d9db2dcf66903d4a082c049b
Ignacio López-Amor <ilopez-amor@ya.com>
2024-06-27 19:26:42 +0200

    feat: fit main screen width

--- src/views/RedirectView.vue
+++ src/views/RedirectView.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts"></script>
 <template>
   <div
-    class="w-full h-screen flex flex-col justify-start content-center items-center mx-2 md:mx-4 lg:mx-5"
+    class="w-fit h-screen flex flex-col justify-start content-center items-center mx-2 md:mx-4 lg:mx-5"
   >
     <h1
       class="pt-6 pb-10 text-sky-950 text-center text-5xl lg:text-7xl font-lucida-black"
     >
       El Tormo
     </h1>
+
     <p
       class="text-sky-800 text-center font-bold font-raleway text-2xl lg:text-4xl"
     >
-      Pronto estaremos de vuelta
+      {{ $t('maintenance.subtitle') }}
     </p>
     <p
       class="pt-4 text-sky-700 text-center font-raleway font-medium text-base lg:text-lg"
     >
-      En El Tormo estamos cocinando los nuevos contenidos de la web para
-      satisfacer a los paladares más exigentes.
+      {{ $t('maintenance.description') }}
     </p>
     <img
       alt="El Tormo logo"
@@ -28,13 +28,13 @@
       class="flex flex-col lg:flex-row justify-center items-center content-center pt-2"
     >
       <p class="text-sky-700 font-raleway text-sm lg:text-base text-center">
-        Para que la espera no se te haga larga puedes reservar una mesa.
+        {{ $t('maintenance.reservations') }}
       </p>
       <div class="flex justify-center items-end">
         <p class="pl-1 text-sky-900 font-bold text-md text-sm lg:text-base">
-          <a href="https://restauranteeltormo.myrestoo.net/es/reservar"
-            >Ir a las reservas</a
-          >
+          <a href="https://restauranteeltormo.myrestoo.net/es/reservar">{{
+            $t('maintenance.to-reservations')
+          }}</a>
         </p>
         <p class="font-material-symbols-outlined text-sm lg:text-base">
           <span>arrow_right_alt</span>
