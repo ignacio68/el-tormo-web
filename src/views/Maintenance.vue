d9f9e836c174439548dea4eca48375244f562856
Ignacio López-Amor <ilopez-amor@ya.com>
2024-06-28 13:50:01 +0200

    bug: fix Maintenance  horizontal centered

--- src/views/Maintenance.vue
+++ src/views/Maintenance.vue
@@ -1,44 +1,61 @@
+// eslint-disable-next-line vue/multi-word-component-names
 <script setup lang="ts"></script>
 <template>
-  <div
-    class="w-fit h-screen flex flex-col justify-start content-center items-center mx-2 md:mx-4 lg:mx-5"
-  >
-    <h1
-      class="pt-6 pb-10 text-sky-950 text-center text-5xl lg:text-7xl font-lucida-black"
-    >
-      El Tormo
-    </h1>
-
-    <p
-      class="text-sky-800 text-center font-bold font-raleway text-2xl lg:text-4xl"
-    >
-      {{ $t('maintenance.subtitle') }}
-    </p>
-    <p
-      class="pt-4 text-sky-700 text-center font-raleway font-medium text-base lg:text-lg"
-    >
-      {{ $t('maintenance.description') }}
-    </p>
-    <img
-      alt="El Tormo logo"
-      class="pt-4 w-[360px] md:w-[400px] lg:w-[600px] grow-0"
-      src="@/assets/illustrations/Restaurant-Chief.svg"
-    />
+  <div class="w-full">
     <div
-      class="flex flex-col lg:flex-row justify-center items-center content-center pt-2"
+      id="content"
+      class="h-screen flex flex-col justify-start content-center items-center mx-2 md:mx-4 lg:mx-5"
     >
-      <p class="text-sky-700 font-raleway text-sm lg:text-base text-center">
-        {{ $t('maintenance.reservations') }}
+      <h1
+        id="title"
+        class="pt-6 pb-10 text-sky-950 text-center text-5xl lg:text-7xl font-lucida-black"
+      >
+        El Tormo
+      </h1>
+
+      <p
+        id="subtitle"
+        class="text-sky-800 text-center font-bold font-raleway text-2xl lg:text-4xl"
+      >
+        {{ $t('maintenance.subtitle') }}
       </p>
-      <div class="flex justify-center items-end">
-        <p class="pl-1 text-sky-900 font-bold text-md text-sm lg:text-base">
-          <a href="https://restauranteeltormo.myrestoo.net/es/reservar">{{
-            $t('maintenance.to-reservations')
-          }}</a>
+      <p
+        class="pt-4 text-sky-700 text-center font-raleway font-medium text-base lg:text-lg"
+      >
+        {{ $t('maintenance.description') }}
+      </p>
+      <img
+        id="logo"
+        alt="El Tormo logo"
+        class="pt-4 w-[360px] md:w-[400px] lg:w-[600px] grow-0"
+        src="@/assets/illustrations/Restaurant-Chief.svg"
+      />
+      <div
+        id="reservations-wrapper"
+        class="flex flex-col lg:flex-row justify-center items-center content-center pt-2"
+      >
+        <p
+          id="reservations"
+          class="text-sky-700 font-raleway text-sm lg:text-base text-center"
+        >
+          {{ $t('maintenance.reservations') }}
         </p>
-        <p class="font-material-symbols-outlined text-sm lg:text-base">
-          <span>arrow_right_alt</span>
-        </p>
+        <div id="to-reservations-wrapper" class="flex justify-center items-end">
+          <p
+            id="to-reservations"
+            class="pl-1 text-sky-900 font-bold text-md text-sm lg:text-base"
+          >
+            <a href="https://restauranteeltormo.myrestoo.net/es/reservar">{{
+              $t('maintenance.to-reservations')
+            }}</a>
+          </p>
+          <p
+            id="to-reservations-icon"
+            class="font-material-symbols-outlined text-sm lg:text-base"
+          >
+            <span>arrow_right_alt</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
